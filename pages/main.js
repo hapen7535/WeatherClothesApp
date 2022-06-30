9c0ced9170d8d30c90d9a8e550548e55f92298d5
Yuhee Kim <dbgml4509@naver.com>
2022-06-30 22:01:02 +0900

    chore : bottomSheet dependencies 추가

--- pages/main.js
+++ pages/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, StatusBar, Image, SafeAreaView, Text } from 'react-native';
+import BottomSheet from "react-native-bottomsheet-reanimated";
 import circles from "../Images/circles.png"
 import person from "../Images/lying.png"
 

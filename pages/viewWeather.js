1a4a8c1fe5ee28204233b8e2ba9527fa388fd7f7
Yuhee Kim <dbgml4509@naver.com>
2022-07-17 13:28:41 +0900

    feat : 날씨 창 배경 도형 그리기

--- pages/viewWeather.js
+++ pages/viewWeather.js
@@ -1,18 +1,34 @@
 import BottomSheet from "react-native-bottomsheet-reanimated";
 import React from 'react';
-import { View, SafeAreaView, Text } from "react-native";
+import { View, SafeAreaView, Text, StyleSheet, StatusBar, Dimensions } from "react-native";
+
+const chartHeight = Dimensions.get('window').height;
+const chartWidth = Dimensions.get('window').width;
 
 export default function ViewWeather (){
     return(
-        // <SafeAreaView>
-        // <Text>
-        // 날씨
-        // </Text>
-        // </SafeAreaView>
-        <View>
-            <Text>
-                eeeee
-            </Text>
-        </View>
+        <SafeAreaView style={styles.background}>
+
+        <StatusBar style="default" />
+        <View style={styles.rectangle}/>
+
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    background : {
+        flex: 1,
+        backgroundColor: "#E9EFC0",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    rectangle : {
+        width: chartWidth-70,
+        height: chartHeight-150,
+        backgroundColor: "#FFFFFF",
+        shadowColor: "#000000", 
+        shadowOpacity: 0.3,//그림자 투명도
+        shadowOffset: { width: 2, height: 2 }, 
+    },
+})
